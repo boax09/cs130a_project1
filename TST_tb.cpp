69f21ef3480e0bc43f46b56923b7f6d7dd135d77
kdotndot <69782047+kdotndot@users.noreply.github.com>
2020-10-19 12:12:22 -0700

    single string gradescope modification

--- TST_tb.cpp
+++ TST_tb.cpp
@@ -1,52 +1,73 @@
 #include <iostream>
 #include "TST.h"
 
+#include <vector>
+
 using namespace std;
 
-//lookup hello, insert goodbye, delete yesterday, insert hello, delete goodbye, range_search band to cat
+//"lookup hello, insert goodbye, delete yesterday, insert hello, delete goodbye, range_search band to cat"
+
 
 
 
 int main(int argc, char* argv[]){
     TST t1;
+    vector<string> allwords;
+
+    string words(argv[1]);
+   
     
-
-    for(int i= 0; i< argc; i++){
-
-        if(string(argv[i]) == "lookup"){
+    string temp = "";
+    for(int i = 0; i < words.size(); i++){
+        if( words[i] == ' ' || words[i] == ','){
             
-            if(t1.lookup(argv[i+1]) == 0){
-                cout<<string(argv[i+1]) + " not found\n";
+            if( temp != "")
+            allwords.push_back(temp);
+            temp = "";
+        } else {
+            temp = temp + words[i];
+
+        }
+    }
+    
+   
+
+    for(int i= 0; i< allwords.size() ; i++){
+
+        if(string(allwords[i]) == "lookup"){
+            
+            if(t1.lookup(allwords[i+1]) == 0){
+                cout<<string(allwords[i+1]) + " not found\n";
                 ++i;
             } else {
-                cout<<string(argv[i+1]) + " found, count = "+ to_string(t1.lookup(argv[i+1]))+ "\n";
+                cout<<string(allwords[i+1]) + " found, count = "+ to_string(t1.lookup(allwords[i+1]))+ "\n";
                 ++i;
             }
 
         }
 
-        if(string(argv[i]) == "insert"){
-            string s = to_string(t1.insert(string(argv[i+1])));
-            cout<<string(argv[i+1]) + " inserted, new count = " + s + "\n";
+        if(string(allwords[i]) == "insert"){
+            string s = to_string(t1.insert(string(allwords[i+1])));
+            cout<<string(allwords[i+1]) + " inserted, new count = " + s + "\n";
             i = i+1;
         }
         
-        if(string(argv[i]) == "delete"){
-            int temp = t1.remove(argv[i+1]);
+        if(string(allwords[i]) == "delete"){
+            int temp = t1.remove(allwords[i+1]);
             if(temp == -1){
                 ++i;
             } else if (temp > 0){
-                cout<< string(argv[i+1]) + " deleted, new count = " + to_string(temp) + "\n";
+                cout<< string(allwords[i+1]) + " deleted, new count = " + to_string(temp) + "\n";
             
             } else {
-                cout<< string(argv[i+1]) + " deleted\n";
+                cout<< string(allwords[i+1]) + " deleted\n";
             }
 
         }
         
-        if(string(argv[i]) == "range_search"){
-            string word1 = string(argv[i+1]);
-            string word2 = string(argv[i+3]);
+        if(string(allwords[i]) == "range_search"){
+            string word1 = string(allwords[i+1]);
+            string word2 = string(allwords[i+3]);
             t1.search(word1,word2);
             i = i+3;
 
